d02c22f90c10de894ecab2df1cddfa2b3f46cab2
zhangchaohui <15001300136@163.com>
2020-03-08 19:30:48 +0800

    update

--- containers/Entrance/Entrance.jsx
+++ containers/Entrance/Entrance.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import classNames from 'classnames'
 import styles from './Entrance.scss'
 
 class Entrance extends React.Component {
@@ -16,35 +16,97 @@ class Entrance extends React.Component {
   }
   componentDidMount = () => {}
   handleMoveLeft = () => {
+    const clickCount = this.state.clickCount === 4 ? 1 : this.state.clickCount += 1
+    const transFormBoxY = this.state.transFormBoxY === -360 ? -90 : this.state.transFormBoxY -= 90
     this.setState({
-      clickCount: this.state.clickCount += 1,
-      transFormBoxY: this.state.transFormBoxY -= 90,
+      clickCount: clickCount,
+      transFormBoxY: transFormBoxY,
+    }, () => {
+      console.log(this.state.transFormBoxY)
+    })
+  }
+  handleMoveRight = () => {
+    const clickCount = this.state.clickCount === 0 ? 3 : this.state.clickCount -= 1
+    const transFormBoxY = this.state.transFormBoxY === 0 ? -270 : this.state.transFormBoxY += 90
+    this.setState({
+      clickCount: clickCount,
+      transFormBoxY: transFormBoxY,
     }, () => {
       console.log(this.state.transFormBoxY)
     })
   }
   render() {
-    const { clickCount } = this.state
+    const { clickCount, transFormBoxY } = this.state
     return (
       <div className={styles.entranceWrapper}>
         <div className={styles.boolLight}>
           <div className={styles.bool} />
           <span className={styles.moveLeft} onClick={this.handleMoveLeft} />
-          <span className={styles.moveRight} />
+          <span className={styles.moveRight} onClick={this.handleMoveRight} />
           <div className={styles.transformBox} style={{ transform: `rotateX(-45deg) rotateY(${this.state.transFormBoxY}deg)` }}>
-            <div className={styles.front} style={{ transform: this.front }}>
-              <div className={styles.sysPic} />
+            <div
+              className={styles.front}
+              style={{ transform: clickCount === 1 ? 'translateZ(400px)' :
+                                  clickCount === 2 ? `${this.front} rotateY(-180deg)`:
+                                  clickCount === 3 ? 'translateZ(400px) rotateY(-90deg)' :
+                                  this.front
+              }}
+            >
+              front
+              <div
+                className={styles.frontSysPic}
+                style={{ transform: clickCount === 1 ? 'rotateY(90deg)' : clickCount === 0 ? 'rotateY(0)' : 'rotateY(0)' }}
+              />
             </div>
-            <div className={styles.back} style={{ transform: this.back }}>
-              <div className={styles.sysPic} />
+            <div
+              className={styles.right}
+              style={{ transform: clickCount === 1 ? 'rotateY(90deg) translateY(-250px)' :
+                                  clickCount === 2 ? `${this.right} rotateY(-180deg)`:
+                                  clickCount === 3 ? 'translateY(250px) rotateY(0)' :
+                                  this.right
+              }}
+            >
+              right
+              <div
+                className={styles.rightSysPic}
+                style={{ transform: clickCount === 0 ? 'rotateY(-90deg)' :
+                                    clickCount === 1 ? 'rotateY(0)' :
+                                    clickCount === 2 ? 'rotateY(-90deg)':
+                                    null
+                }}
+              />
             </div>
-            <div className={styles.left} style={{ transform: this.left }}>
-              <div className={styles.sysPic} />
+            <div
+              className={styles.back}
+              style={{ transform: clickCount === 1 ? 'translateZ(-400px)' :
+                                  clickCount === 2 ? `${this.back} rotateY(-180deg)`:
+                                  clickCount === 3 ? 'translateZ(-400px) rotateY(-90deg)':
+                                  this.back
+              }}
+            >
+              back
+              <div
+                className={styles.backSysPic}
+                style={{ transform: clickCount === 1 ? 'rotateY(90deg)' : clickCount === 0 ? 'rotateY(0)' : 'rotateY(0)' }}
+              />
             </div>
-            <div className={styles.right} style={{ transform: this.right }}>
-              <div className={styles.sysPic} />
+            
+            <div
+              className={styles.left}
+              style={{ transform: clickCount === 1 ? 'rotateY(90deg) translateY(250px)' :
+                                  clickCount === 2 ? `${this.left} rotateY(-180deg)`:
+                                  clickCount === 3 ? 'translateY(-250px) rotateY(0)':
+                                  this.left
+              }}
+            >
+              left
+              <div
+                className={styles.leftSysPic}
+                style={{ transform: clickCount === 0 ? 'rotateY(-90deg)' : clickCount === 1 ? 'rotateY(0)' : 'rotateY(-90deg)' }}
+              />
             </div>
           </div>
+          
         </div>
       </div>
     )
