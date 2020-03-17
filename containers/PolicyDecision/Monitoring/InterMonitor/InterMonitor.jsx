99ef4b00589a1fee5b387504b2d578ff0255cf94
zhangchaohui <15001300136@163.com>
2020-03-17 11:31:41 +0800

    update

--- containers/PolicyDecision/Monitoring/InterMonitor/InterMonitor.jsx
+++ containers/PolicyDecision/Monitoring/InterMonitor/InterMonitor.jsx
@@ -51,42 +51,52 @@ class InterMonitor extends React.Component {
   render() {
     const { statusName } = this.state
     return (
-      <div className={styles.interMonitorBox}>
-        <span className={styles.hideIcon}><Icon type="backward" /></span>
-        <div className={styles.title}>路口监视功能</div>
-        <div className={styles.interMsg}>
-          <div className={styles.msgShow}>
-            <div className={styles.listIcon} />
-            <p>系统路口列表</p>
+      <React.Fragment>
+        <div className={styles.interMonitorBox}>
+          <span className={styles.hideIcon}><Icon type="backward" /></span>
+          <div className={styles.title}>路口监视功能</div>
+          <div className={styles.interMsg}>
+            <div className={styles.msgShow}>
+              <div className={styles.listIcon} />
+              <p>系统路口列表</p>
+            </div>
+            <div className={styles.msgShow}>
+              <div className={styles.analysisIcon} />
+              <p>整体运行分析</p>
+            </div>
           </div>
-          <div className={styles.msgShow}>
-            <div className={styles.analysisIcon} />
-            <p>整体运行分析</p>
+          <div className={styles.title}>状态监视</div>
+          <div className={styles.statusMsg}>
+            <DropDownList
+              title="控制状态监视"
+              list={this.statusList}
+              handleClick={this.handleStatusDropDown}
+              statusName="control"
+              isShow={statusName === 'control'}
+            />
+            <DropDownList
+              title="交通状态监视"
+              handleClick={this.handleStatusDropDown}
+              statusName="traffic"
+              isShow={statusName === 'traffic'}
+            />
+            <DropDownList
+              title="设备状态监视"
+              handleClick={this.handleStatusDropDown}
+              statusName="device"
+              isShow={statusName === 'device'}
+            />
           </div>
         </div>
-        <div className={styles.title}>状态监视</div>
-        <div className={styles.statusMsg}>
-          <DropDownList
-            title="控制状态监视"
-            list={this.statusList}
-            handleClick={this.handleStatusDropDown}
-            statusName="control"
-            isShow={statusName === 'control'}
-          />
-          <DropDownList
-            title="交通状态监视"
-            handleClick={this.handleStatusDropDown}
-            statusName="traffic"
-            isShow={statusName === 'traffic'}
-          />
-          <DropDownList
-            title="设备状态监视"
-            handleClick={this.handleStatusDropDown}
-            statusName="device"
-            isShow={statusName === 'device'}
-          />
+        <div className={styles.pointTypeBox}>
+          <div className={styles.title}>系统点位分布类型</div>
+          <div className={styles.systemPoint}>
+            <div><span className={styles.upIconBox}><Icon type="up" /></span>海信系统<span><Checkbox defaultChecked /></span></div>
+            <div><span className={styles.squareBox} />ATC系统<span><Checkbox defaultChecked /></span></div>
+            <div><span className={styles.circleBox} />泰尔文特<span><Checkbox defaultChecked /></span></div>
+          </div>
         </div>
-      </div>
+      </React.Fragment>
     )
   }
 }
