e239d05060a30fc419d7b46110846c9785f52d2c
qiaoshuai <1522740101@qq.com>
2020-03-21 18:31:48 +0800

    'update'

--- containers/PolicyDecision/AreaOptimize/AreaOptimize.jsx
+++ containers/PolicyDecision/AreaOptimize/AreaOptimize.jsx
@@ -6,6 +6,7 @@ import CustomTree from './CustomTree/CustomTree'
 import EchartsPage from '../../../components/ecahrtsPage/EchartsPage'
 import echartss from './chartsOptions'
 import Intersection from './IntersectionList/Intersection'
+import InfoBg from './img/Infobg.png'
 
 class AreaOptimize extends React.Component {
   constructor(props) {
@@ -24,24 +25,116 @@ class AreaOptimize extends React.Component {
         name: '区域平均车速',
       },
     ]
+    this.marker = null
+    this.infowindow = 0
   }
 
-  componentDidMount = () => { }
-
+  componentDidMount = () => {
+    this.renderMineMap()
+  }
   onChange = () => { // 协调方向切换
 
+  }
+  // 添加坐标点
+  addMarker = () => {
+    if (this.map) {
+      this.infowindow += 1
+      const el = document.createElement('div')
+      el.id = 'marker'
+      // el.style['background-image'] = 'url(/api/static/demo/js-api/zh/images/park.png)'
+      el.style['background-color'] = '#ff0000'
+      el.style['background-size'] = 'cover'
+      el.style.width = '20px'
+      el.style.height = '20px'
+      el.style['border-radius'] = '50%'
+      this.lnglat = this.map.getCenter()
+      this.marker = new window.minemap.Marker(el, { offset: [-25, -25] })
+        .setLngLat(this.lnglat)
+        .setPopup(this.showInterInfo())
+        .addTo(this.map)
+      // el.addEventListener('click', this.showInterInfo)
+    }
+  }
+  // 删除坐标点
+  delMarker = () => {
+    if (this.map && this.marker) {
+      this.marker.remove()
+      this.marker = null
+    }
+  }
+  // 更新坐标点
+  updateMarkerPosition = () => {
+    if (this.map && this.marker) {
+      const lnglat = this.map.getCenter()
+      this.marker.setLngLat([lnglat.lng + 0.01, lnglat.lat + 0.01])
+    }
+  }
+  // 关闭自定义信息窗体
+  removeInterInfo = () => {
+    if (this.popup) {
+      this.popup.remove()
+      this.popup = null
+    }
+  }
+  // 自定义信息窗体
+  showInterInfo = () => {
+    this.removeInterInfo()
+    const lnglat = this.map.getCenter()
+    const id = `removeInterInfo${this.infowindow}`
+    const infoHtml = `
+      <div style="width:480px;height:260px;background:url(${InfoBg}) center center no-repeat;background-size:100% 100%;">
+        <div style="position:relative;height:50px;padding-top:13px;padding-left:20px;line-height:50px;font-size:15px;">
+          路口名称 ：123456
+          <span id=${id} style="position:absolute;top:25px;right:25px;width:20px;height:20px;text-align:center;line-height:20px;font-size:16px;cursor:pointer;color:#49C2D5;">X</span>
+        </div>
+        <div style="height:200px;display:flex;padding-top:20px;font-size:14px;">
+          <div style="flex:1;">
+            <p style="height:32px;line-height:32px;padding-left:40px">所属城区 ：兴宁区</p>
+            <p style="height:32px;line-height:32px;padding-left:40px">信号系统 ：海信</p>
+            <p style="height:32px;line-height:32px;padding-left:40px">运行阶段 ：东西左转</p>
+            <div style="width:80px;height:30px;margin:20px auto 0;background-color:#0F85FF;text-align:center;line-height:30px;border-radius:4px; cursor: pointer;">路口监控</div>
+          </div>
+          <div style="flex:1;">
+            <p style="height:32px;line-height:32px;padding-left:20px">控制状态 ：本地多时段</p>
+            <p style="height:32px;line-height:32px;padding-left:20px">信号机IP ：192.168.1.204</p>
+            <p style="height:32px;line-height:32px;padding-left:20px">设备状态 ：正常</p>
+            <div style="width:80px;height:30px;margin:20px auto 0;background-color:#0F85FF;text-align:center;line-height:30px;border-radius:4px; cursor: pointer;">路口优化</div>
+          </div>
+        </div>
+      </div>
+    `
+    this.popup = new window.minemap.Popup({ closeOnClick: true, closeButton: false, offset: [-15, -25] })
+      .setLngLat([lnglat.lng, lnglat.lat])
+      .setHTML(infoHtml)
+      .addTo(this.map)
+    document.getElementById(id).addEventListener('click', this.removeInterInfo)
+    return this.popup
   }
   btnRegion = (id) => { // 切点击区域
     this.setState({
       num: id,
     })
   }
+  // 初始化地图
+  renderMineMap = () => {
+    const map = new window.minemap.Map({
+      container: 'mapContainer',
+      style: '//minedata.cn/service/solu/style/id/2301',
+      center: [106.709075, 26.586574],
+      zoom: 14,
+      pitch: 0,
+      maxZoom: 17,
+      minZoom: 3,
+    })
+    this.map = map
+    this.addMarker()
+  }
   render() {
     const { Option } = Select
     const { num } = this.state
     console.log(num)
     return (
-      <div className={styles.areaOptWrapper}>
+      <div className={styles.areaOptWrapper} id="mapContainer">
         <Header {...this.props} />
         <div className={styles.areaOptContainer}>
           <div className={styles.interTreeBox}>
