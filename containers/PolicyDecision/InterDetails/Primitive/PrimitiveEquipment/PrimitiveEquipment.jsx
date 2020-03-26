6de42503a92f1b096da0a21f90d3f964556d08b9
qiaoshuai <1522740101@qq.com>
2020-03-26 17:02:05 +0800

    'update'

---
+++ containers/PolicyDecision/InterDetails/Primitive/PrimitiveEquipment/PrimitiveEquipment.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import styles from './PrimitiveEquipment.scss'
+
+class PrimitiveEquipment extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  componentDidMount() {
+  }
+  render() {
+    return (
+      <div className={styles.PrimitiveEquipmentbox}>
+        1
+      </div>
+    )
+  }
+}
+
+export default PrimitiveEquipment
