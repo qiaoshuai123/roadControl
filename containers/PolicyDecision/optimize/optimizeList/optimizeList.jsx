c295b7073ee20bb9f736cf4f14abe38af324a444
zhangchaohui <15001300136@163.com>
2020-05-09 10:55:04 +0800

    update

--- containers/PolicyDecision/optimize/optimizeList/optimizeList.jsx
+++ containers/PolicyDecision/optimize/optimizeList/optimizeList.jsx
@@ -20,10 +20,10 @@ class OptimizeList extends Component {
         </div>
         <div className={styles.mountingTbody}>
           <div className={styles.mountingTr}>
-            <div className={styles.mountingTd}>*************</div>
-            <div className={styles.mountingTd}>*************</div>
-            <div className={styles.mountingTd}>*************</div>
-            <div className={styles.mountingTd}>*************</div>
+            <div className={styles.mountingTd}>0.2</div>
+            <div className={styles.mountingTd}>C</div>
+            <div className={styles.mountingTd}>686</div>
+            <div className={styles.mountingTd}>4207</div>
           </div>
           <div className={styles.mountingThead}>
             <div className={styles.mountingTh}>交叉口饱和度</div>
@@ -31,9 +31,9 @@ class OptimizeList extends Component {
             <div className={styles.mountingTh}>交叉口通行流量</div>
           </div>
           <div className={styles.mountingTr}>
-            <div className={styles.mountingTd}>*************</div>
-            <div className={styles.mountingTd}>*************</div>
-            <div className={styles.mountingTd}>*************</div>
+            <div className={styles.mountingTd}>0.6</div>
+            <div className={styles.mountingTd}>0.44</div>
+            <div className={styles.mountingTd}>0.16</div>
           </div>
           <div className={styles.mountingThead}>
             <div className={styles.mountingTh}>交叉口饱和度</div>
@@ -41,9 +41,9 @@ class OptimizeList extends Component {
             <div className={styles.mountingTh}>交叉口通行流量</div>
           </div>
           <div className={styles.mountingTr}>
-            <div className={styles.mountingTd}>*************</div>
-            <div className={styles.mountingTd}>*************</div>
-            <div className={styles.mountingTd}>*************</div>
+            <div className={styles.mountingTd}>7093</div>
+            <div className={styles.mountingTd}>18000</div>
+            <div className={styles.mountingTd}>0.3</div>
           </div>
         </div>
       </div>
